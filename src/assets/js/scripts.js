eefd33194999db5c532bb9c22c9a3c831c5eea69
Jorge S Junior <jorgeserranojunior@hotmail.com>
2021-02-17 13:50:52 -0300

    whatsapp

--- src/assets/js/scripts.js
+++ src/assets/js/scripts.js
@@ -312,16 +312,23 @@ function isMobile() {
 }
 
 function verifyDevice() {
-  const whatsapp = document.querySelectorAll(".whatsapp");
+  let whatsapp = document.querySelectorAll(".whatsapp");
+  console.log(whatsapp);
+
   if (isMobile()) {
     whatsapp.forEach((item) => {
-      item = item.href = "whatsapp://send?phone=5511946439695";
+      item.href = "whatsapp://send?phone=5511946439695";
+      console.log(item.href);
     });
+    return whatsapp;
   } else {
     whatsapp.forEach((item) => {
       item = item.href = "https://web.whatsapp.com/send?phone=5511946439695";
+      console.log(item.href);
     });
+    return whatsapp;
   }
 }
 
 verifyDevice();
+// console.log(verifyDevice());
