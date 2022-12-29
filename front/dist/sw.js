4dcb38de7e8f275566a33019e2389676debd38c7
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2022-12-29 16:20:32 -0300

    pwa

--- front/dist/sw.js
+++ front/dist/sw.js
@@ -1,8 +1,25 @@
-var CACHE_NAME = "v1.3.7",
-  urlsToCache = ["/"];
-self.addEventListener("install", function (e) {
+const cacheName = "js13kPWA-v1";
+const contentToCache = ["/"];
+
+/* self.addEventListener("install", function (e) {
   e.waitUntil(caches.open(CACHE_NAME).then((e) => e.addAll(urlsToCache)));
 }),
   self.addEventListener("fetch", (e) => {
     e.respondWith(caches.match(e.request).then((s) => s || fetch(e.request)));
   });
+ */
+
+self.addEventListener("install", (e) => {
+  console.log("[Service Worker] Install");
+  e.waitUntil(
+    (async () => {
+      const cache = await caches.open(cacheName);
+      console.log("[Service Worker] Caching all: app shell and content");
+      await cache.addAll(contentToCache);
+    })()
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  console.log(`[Service Worker] Fetched resource ${e.request.url}`);
+});
--- front/src/main.ts
+++ front/src/main.ts
@@ -1,10 +1,10 @@
 import portfolio from "./assets/scripts/portfolio";
 import isMobile from "./assets/scripts/isMobile";
-/* import pwa from "./assets/scripts/pwa";
- */ import togleMenu from "./assets/scripts/togleMenu";
+import pwa from "./assets/scripts/pwa";
+import togleMenu from "./assets/scripts/togleMenu";
 import btnToTop from "./assets/scripts/btnToTop";
 isMobile();
 portfolio();
-/* pwa();
- */ togleMenu();
+pwa();
+togleMenu();
 btnToTop();
