57dccc9e1dfbdc2959a86efbe2ffadc466f2331f
Jorge Serrano Junior <jorgeserranojunior@hotmail.com>
2022-12-29 00:28:46 -0300

    2022

---
+++ front/src/assets/scripts/pagesImports.ts
@@ -0,0 +1,11 @@
+import { insertHtml } from "./insertHtml";
+
+/* insertHtml("./src/components/header.html", "#header");
+ */ insertHtml("./src/components/banner.html", "#banner");
+insertHtml("./src/components/sistemas.html", "#sistemas");
+insertHtml("./src/components/servicos.html", "#servicos");
+insertHtml("./src/components/desenvolvedor.html", "#desenvolvedor");
+insertHtml("./src/components/tecnologias.html", "#tecnologias");
+insertHtml("./src/components/portfolio.html", "#portfolio");
+
+export {};
---
+++ front/src/main.ts
@@ -0,0 +1,8 @@
+import portfolio from "./assets/scripts/portfolio";
+import isMobile from "./assets/scripts/isMobile";
+import pwa from "./assets/scripts/pwa";
+import togleMenu from "./assets/scripts/togleMenu";
+isMobile();
+portfolio();
+pwa();
+togleMenu();
