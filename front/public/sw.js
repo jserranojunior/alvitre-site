51649f4b0a9752f0594ea164acf0ded734391bb3
Jorge <jorgeserranojunior@gmail.com>
2023-11-02 11:03:14 -0300

    pwa

--- front/public/sw.js
+++ front/public/sw.js
@@ -1,8 +1,8 @@
-const cacheName = "js13kPWA-v2.4";
+const cacheName = "js13kPWA-v2.6.3";
 const contentToCache = ["/"];
 
 self.addEventListener("install", (event) => {
-  console.log("Service Worker started");
+  console.log("Cache online");
   event.waitUntil(
     (async () => {
       const cache = await caches.open(cacheName);
@@ -15,20 +15,25 @@ self.addEventListener("fetch", (event) => {
   event.respondWith(
     (async () => {
       const cache = await caches.open(cacheName);
-      const cachedResponse = await cache.match(event.request);
-      if (cachedResponse) {
-        return cachedResponse;
-      } else {
-        const networkResponse = await fetch(event.request);
-        if (
-          networkResponse &&
-          networkResponse.status === 200 &&
-          networkResponse.type === "basic"
-        ) {
-          const clonedResponse = networkResponse.clone();
-          cache.put(event.request, clonedResponse);
+      try {
+        const cachedResponse = await cache.match(event.request);
+        if (cachedResponse) {
+          return cachedResponse;
         }
+  
+        const networkResponse = await fetch(event.request);
+  
+        if (!networkResponse || networkResponse.status !== 200 || networkResponse.type !== "basic") {
+          return networkResponse;
+        }
+  
+        const clonedResponse = networkResponse.clone();
+        await cache.put(event.request, clonedResponse);
+  
         return networkResponse;
+      } catch (error) {
+        console.error("Erro ao lidar com a solicitação de cache:", error);
+        throw error;
       }
     })()
   );
--- front/src/assets/scripts/pwa.ts
+++ front/src/assets/scripts/pwa.ts
@@ -1,50 +1,61 @@
 export default () => {
+
+  let myPrompt: any;
+  let swRegistration: ServiceWorkerRegistration | null = null;
+
+
+  if ("serviceWorker" in navigator && "Notification" in window) {
+    navigator.serviceWorker.register("sw.js")
+      .then((registration) => {
+        console.log("Cache registrado com sucesso, disponível para instalação");
+        swRegistration = registration;
+      })
+      .catch(() => {
+        console.log("Erro ao registrar o cache");
+      });
+  } else {
+    console.log("CLIENTE: O service worker ou notificações não são suportados.");
+  }
+
+   /* Altura da notificação*/
   const headerHeight = document.querySelector<HTMLDivElement>("#header");
   const toastNotifications = document.querySelector<HTMLDivElement>("#toastNotifications");
 
   if (toastNotifications && headerHeight) {
     toastNotifications.style.top = `${headerHeight.offsetHeight + 25}px`;
   }
+  /* ##################### */
 
-  if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js")
-      .then(() => {
-        console.log("PWA registrado com sucesso, disponível para instalação");
-      })
-      .catch(() => {
-        console.log("Erro ao registrar o PWA");
-      });
-  } else {
-    console.log("CLIENTE: O service worker não é suportado.");
-  }
 
-  let myPrompt: any;
+ /*  Verifica se existe o botão para instação e adiciona o evento nele*/
   const pwaAlert = document.querySelector<HTMLDivElement>(".div-install-pwa");
-  const btnPWA = document.querySelector(".btn-install-pwa");
-  const btnPWAClose = document.querySelector(".btn-pwa-close");
-
   if (pwaAlert) {
     const beforeInstallPromptHandler = (e: Event) => {
       e.preventDefault();
       myPrompt = e;
       pwaAlert.style.display = "block";
     };
-
     window.addEventListener("beforeinstallprompt", beforeInstallPromptHandler);
   }
+  /* ##################### */
 
+
+ /* Ao clicar ele exibe a tela de instatação e oculta o botão*/
+  const btnPWA = document.querySelector(".btn-install-pwa");
   if (btnPWA) {
     btnPWA.addEventListener("click", () => {
       if (pwaAlert) {
         pwaAlert.style.display = "none";
       }
-
       if (myPrompt) {
         myPrompt.prompt();
       }
     });
   }
+  /* ##################### */
 
+ /* Cancela a instalação e oculta o botão*/
+  const btnPWAClose = document.querySelector(".btn-pwa-close");
   if (btnPWAClose) {
     btnPWAClose.addEventListener("click", () => {
       if (pwaAlert) {
@@ -52,4 +63,54 @@ export default () => {
       }
     });
   }
+   /* ##################### */
+
+
+    /* Adicionar código para solicitar permissões de notificação */
+  const requestNotificationPermission = async () => {
+    try {
+      const permission = await Notification.requestPermission();
+      if (permission === "granted") {
+        console.log("Permissão de notificação concedida");
+
+  showNotification();
+      } else {
+        console.log("Permissão de notificação negada");
+      }
+    } catch (error) {
+      console.error("Erro ao solicitar permissão de notificação:", error);
+    }
+  };
+
+  // Chamar a função acima em algum ponto do seu código, por exemplo, quando a página carrega.
+  requestNotificationPermission();
+
+
+  function showNotification (){
+    console.log("functionandio")
+    if (swRegistration) {
+/*       const options = {
+        body: "Agradecemos pela inscrição",
+        icon: "/static/logo/png/alvitre.png",
+      }; */
+
+      const options = {
+        body: 'Bem-vindo ao nosso serviço!',
+        icon: '/static/logo/png/alvitre.png',
+        image: '/static/logo/png/logo-alvitre-dark-mode-retangulo-arrendodado.png',
+        badge: '/static/logo/png/logo-circulo-de-fundo.png',
+        actions: [
+          { action: 'close', title: 'Fechar' }
+        ],
+        requireInteraction: true,
+        silent: false,
+        timestamp: Date.now()
+      };
+      
+  
+      swRegistration.showNotification("Obrigado por se inscrever", options);
+    }
+  };
+
+
 };
